38e580fcda5fd85e63dce9ae1f4a8849ff19f553
Regnareb <kun_fu_man@hotmail.com>
2013-10-18 10:41:16 +0200

    Create timelapse.py

---
+++ timelapse.py
@@ -0,0 +1,7 @@
+'''
+- Create the different cameras if they do not exist (1 front, 2 opposite three-quarter profile, 2 moving cameras each (upside-down/middle/bottom-up))
+- every X seconds render with the hardware rendered with each cameras
+- rotate around the object with some cameras
+- zoom-out from the object when it grows (with a smooth transition)
+- save the scene every X seconds to do batch renders later. See if the saving is fast enough and does not interrupt the user's work.
+'''
