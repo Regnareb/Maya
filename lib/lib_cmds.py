4728e0431d71a917ad648c87444a85d1e421f2df
Regnareb <kun_fu_man@hotmail.com>
2014-05-22 20:00:47 +0200

    Update lib_cmds.py

--- lib/lib_cmds.py
+++ lib/lib_cmds.py
@@ -2,7 +2,7 @@ import maya.mel as mel
 import maya.cmds as cmds
 
 
-def getNewNodesCreated(_function):
+def getNewNodesCreated( _function ):
     """ Return the new nodes created after the execution of a function """
     before = cmds.ls()
     eval(_function)
@@ -10,7 +10,7 @@ def getNewNodesCreated(_function):
     return list(set(after) - set(before))
 
 
-def getNumberCVs(curve):
+def getNumberCVs( curve ):
     """Return the number of CVs of an input curve"""
     numSpans = cmds.getAttr ( curve + ".spans" )
     degree   = cmds.getAttr ( curve + ".degree" )
@@ -21,7 +21,26 @@ def getNumberCVs(curve):
     return numCVs
 
 
-def getTransforms(shapeList, fullPath=True):
+def getNurbsCVs( surface ):
+    """Return the number of CVs in U and V of the NURBS surface in argument"""
+    numSpansU = cmds.getAttr ( surface + ".spansU" )
+    degreeU   = cmds.getAttr ( surface + ".degreeU" )
+    numSpansV = cmds.getAttr ( surface + ".spansV" )
+    degreeV   = cmds.getAttr ( surface + ".degreeV" )
+    formU     = cmds.getAttr ( surface + ".formU" )
+    formV     = cmds.getAttr ( surface + ".formV" )
+    numCVsU   = numSpansU + degreeU
+    #Adjust for periodic hull:
+    if ( formU == 2 ):
+        numCVsU -= degreeU
+    #Adjust for periodic hull:
+    if ( formV != 2 ):
+        numCVsV = numSpansV + degreeV
+
+    return numCVsU, numCVsV
+
+
+def getTransforms( shapeList, fullPath=True ):
     """Return all the transforms of the list of shapes in argument"""
     transforms = []
     for node in shapeList:
@@ -29,12 +48,6 @@ def getTransforms(shapeList, fullPath=True):
             parent = cmds.listRelatives( node, fullPath=fullPath, parent=True )
             transforms.append( parent[0] )
     return transforms
-
-
-def getTypeNode(type, nodeList):
-    """ Return all the nodes of a specific type from a list of node """
-    filter = cmds.itemFilter( byType=type )
-    return cmds.lsThroughFilter(filter, item=nodeList)
 
 
 def getShapes( xform ):
@@ -45,21 +58,48 @@ def getShapes( xform ):
     return shapes
 
 
-def logException(type, value, trace):
-    """ Print info on exception: sys.excepthook = logException """
-    print(pprint.pformat(inspect.currentframe().f_locals))
-    print(pprint.pformat(inspect.currentframe().f_builtins))
-    print(pprint.pformat(inspect.currentframe().f_globals))
-    print("\n\n%s"%(''.join(traceback.format_exception(type, value, trace))))
+def getTypeNode( type, nodeList ):
+    """ Return all the nodes of a specific type from a list of node """
+    filter = cmds.itemFilter( byType=type )
+    return cmds.lsThroughFilter(filter, item=nodeList)
 
 
-def undo_chunk( method ):
+def getFirstSelection( filter='' ):
+    """Get the first item in the selection"""
+    selection = cmds.ls(selection=True)
+    if filter and selection:
+        selection = cmds.filterExpand( selection, sm=filter)
+    if selection:
+        return selection[0]
+    else:
+        return ''
+
+
+
+
+
+
+
+def undoChunk( method ):
     """A decorator that create a undo chunk so that everything done in the method will be undone with only one Undo"""
     def undoed( *args, **kw ):
-        cmds.undoInfo( openChunk=True )
-        result = method( *args, **kw )
-        cmds.undoInfo( closeChunk=True )
+        try:
+            cmds.undoInfo( openChunk=True )
+            result = method( *args, **kw )
+        except Exception as err:
+            print err
+        finally:
+            cmds.undoInfo( closeChunk=True )
     return undoed
-    
-    
-    
+
+
+
+
+class UndoContext(object):
+    def __enter__(self):
+        cmds.undoInfo(openChunk=True)
+    def __exit__(self, *exc_info):
+        cmds.undoInfo(closeChunk=True)
+
+# with UndoContext():
+#     ... your code here....
