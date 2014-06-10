2d238ef33049a06ea950b82e4bd5e2f649b7d41a
Regnareb <kun_fu_man@hotmail.com>
2014-06-10 13:05:03 +0200

    Option to log only specific users
    Specify in the argument a list of users you want to log. If nothing is specified, every user will have logs.

--- createLogs.py
+++ createLogs.py
@@ -2,21 +2,24 @@ import os
 import time
 import maya.cmds as cmds
 
-def createLogs():
+def createLogs(listUsers=[]):
     """
-    Creates a log file in the maya prefs folder recording everything printed in the Command File Output.
+    Creates a log file in the maya prefs folder, recording everything printed in the Command File Output
+    If nothing is specified for the argument 'listUser', it will do it for everyone
     """
-    mayaVersion     = cmds.about( version=True )
-    folderYearMonth = time.strftime( '%Y-%m' )
-    timeNow         = time.strftime( '%Y-%m-%d__%H:%M:%S' )
-    fileName        = timeNow + '.log'
-    folderPath      = os.path.join( os.environ['MAYA_APP_DIR'], 'mayalogs', mayaVersion.replace(' ', '-'), folderYearMonth )
-    filePath         = os.path.join( folderPath, fileName )
+    userLogin  = os.environ['USER']
+    if userLogin in listUsers or not listUsers:
+        mayaVersion = cmds.about( version=True )
+        yearMonth   = time.strftime( '%Y-%m' )
+        timeNow     = time.strftime( '%Y-%m-%d__%H:%M:%S' )
+        fileName    = timeNow + '.log'
+        folderPath  = os.path.join( os.environ['MAYA_APP_DIR'], 'mayalogs', mayaVersion.replace(' ', '-'), yearMonth )
+        filePath    = os.path.join( folderPath, fileName )
 
-    try:
-        os.makedirs( folderPath )
-    except OSError as exception:
-            if exception.errno != errno.EEXIST:
-                raise
+        try:
+            os.makedirs( folderPath )
+        except OSError as exception:
+                if exception.errno != errno.EEXIST:
+                    raise
 
-    cmds.cmdFileOutput( open=filePath )
+        cmds.cmdFileOutput( open=filePath )
