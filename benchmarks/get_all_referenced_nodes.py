91ab43a0dc463113d82d0d7fc03f80ef0007a226
Regnareb <kun_fu_man@hotmail.com>
2014-11-10 16:33:56 +0100

    Create get_all_referenced_nodes.py

---
+++ benchmarks/get_all_referenced_nodes.py
@@ -0,0 +1,21 @@
+import timeit
+num = 10
+
+s = """
+import maya.cmds as cmds
+allNodes = cmds.ls()
+[i for i in allNodes if cmds.referenceQuery(i, isNodeReferenced=True)]
+"""
+print timeit.timeit(s, number=num)
+
+s = """
+import maya.cmds as cmds
+cmds.ls(rn=True)
+""" 
+print timeit.timeit(s, number=num)
+
+# Results
+# 11.343184948
+# 2.8212761879
+
+
