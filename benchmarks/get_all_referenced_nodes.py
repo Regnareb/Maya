d5758390af8c3e727790ecd4f3783d46f916c931
Regnareb <kun_fu_man@hotmail.com>
2014-11-10 16:58:21 +0100

    Update get_all_referenced_nodes.py

--- benchmarks/get_all_referenced_nodes.py
+++ benchmarks/get_all_referenced_nodes.py
@@ -1,21 +1,46 @@
 import timeit
 num = 10
+nodeList = cmds.ls() 
 
 s = """
 import maya.cmds as cmds
-allNodes = cmds.ls()
-[i for i in allNodes if cmds.referenceQuery(i, isNodeReferenced=True)]
-"""
+nodeList = %s
+[i for i in nodeList if cmds.ls(i, rn=True)]
+""" % nodeList
 print timeit.timeit(s, number=num)
 
 s = """
 import maya.cmds as cmds
+nodeList = %s
+[i for i in nodeList if cmds.referenceQuery(i, isNodeReferenced=True)]
+""" % nodeList
+print timeit.timeit(s, number=num)
+
+s = """
+import maya.cmds as cmds
+nodeList = %s
+cmds.ls(nodeList, referencedNodes=True)
+""" % nodeList
+print timeit.timeit(s, number=num)
+
+s = """
+import maya.cmds as cmds
+nodeList = %s
 cmds.ls(referencedNodes=True)
-""" 
+""" % nodeList
 print timeit.timeit(s, number=num)
 
-# Results
-# 11.343184948
-# 2.8212761879
 
+# Results on huge list:
+# 15.5261850357
+# 10.0199069977
+# 5.2493159771
+# 2.59991717339
 
+# Results on small list:
+# 0.107467889786
+# 0.0747890472412
+# 0.0295581817627
+# 2.61482691765
+
+# equal when nodeList[0:7200] on 13911 total nodes
