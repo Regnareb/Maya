3e960eda99acfba50046e94404a80290da7007d6
Regnareb <kun_fu_man@hotmail.com>
2014-11-12 17:30:49 +0100

    Update string_to_booleans.py

--- benchmarks/string_to_booleans.py
+++ benchmarks/string_to_booleans.py
@@ -1,7 +1,6 @@
 import timeit
 num = 100000
-string = 'False'
-
+string = 'True'
 
 s = """
 import ast
@@ -15,13 +14,44 @@ string2bool(%s)
 print timeit.timeit(s, number=num)
 
 
-
 s = """
 def string2bool(string):
     dico = {'True':True, 'False':False}
     try:
         return dico[string]
     except KeyError:
+        return string
+string2bool(%s)
+""" % string
+print timeit.timeit(s, number=num)
+
+
+s = """
+def string2bool(string):
+    d = {"True":True, "False":False}
+    return d.get(string, string)
+string2bool(%s)
+""" % string
+print timeit.timeit(s, number=num)
+
+
+s = """
+def string2bool(string):
+    string = False if string == 'False' else string
+    string = True if string == 'True' else string
+    return string
+string2bool(%s)
+""" % string
+print timeit.timeit(s, number=num)
+
+
+s = """
+def string2bool(string):
+    if string == 'False':
+        return False
+    elif string == 'True':
+        return True
+    else:
         return string
 string2bool(%s)
 """ % string
@@ -37,35 +67,60 @@ print timeit.timeit(s, number=num)
 
 
 
+# Only boolean
+
+import timeit
+num = 100000
+string = 'True'
+
 s = """
+import ast
 def string2bool(string):
-    d = {"True":True, "False":False}
-    return d.get(string, string)
-string2bool(%s)
+    ast.literal_eval(string)
+string2bool('%s')
 """ % string
 print timeit.timeit(s, number=num)
 
 
-
 s = """
 def string2bool(string):
-    string = False if string == 'False' else string
-    string = True if string == 'True' else string
-    return string
-string2bool(%s)
+    dico = {'True':True, 'False':False}
+    return dico[string]
+string2bool('%s')
 """ % string
 print timeit.timeit(s, number=num)
-
 
 
 s = """
 def string2bool(string):
     if string == 'False':
         return False
-    elif string == 'True':
-        return True
     else:
-        return string
+        return True
 string2bool(%s)
 """ % string
 print timeit.timeit(s, number=num)
+
+
+s = """
+def string2bool(string):
+    return False if string == 'False' else True
+string2bool(%s)
+""" % string
+print timeit.timeit(s, number=num)
+
+
+s = """
+def string2bool(string):
+    return string == "True"
+string2bool(%s)
+""" % string
+print timeit.timeit(s, number=num)
+
+
+# Results
+# 1.19827890396
+# 0.0405788421631
+# 0.0305030345917
+# 0.030061006546
+# 0.028342962265
