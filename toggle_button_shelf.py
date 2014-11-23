57895f70d36a1d408de858c6225f870c580ffab9
Regnareb <kun_fu_man@hotmail.com>
2014-11-23 18:45:12 +0100

    Update toggle_button_shelf.py

--- toggle_button_shelf.py
+++ toggle_button_shelf.py
@@ -1,21 +1,21 @@
 def toggle_button():
-    toggle_button = ''
+    toggle_btn = ''
     top_shelf = mel.eval("$dummy = $gShelfTopLevel")
     current_tab = cmds.tabLayout(top_shelf, query=True, selectTab=True)
     shelf_buttons = cmds.shelfLayout(current_tab, query=True, childArray=True)
     for button in shelf_buttons:
         cmd = cmds.shelfButton(button, query=True, command=True)
         if "toggle_button()" in cmd:
-            toggle_button = button
+            toggle_btn = button
             break
     
     status = cmds.pluginInfo('Substance', query=True, loaded=True)
-    if status and toggle_button:
+    if status and toggle_btn:
         cmds.unloadPlugin('Substance')
-        cmds.shelfButton(toggle_button, edit=True, backgroundColor=(1, 0, 0))
-    elif toggle_button:  
+        cmds.shelfButton(toggle_btn, edit=True, backgroundColor=(1, 0, 0))
+    elif toggle_btn:  
         cmds.loadPlugin ('Substance')
-        cmds.shelfButton(toggle_button, edit=True, backgroundColor=(0, 1, 0))
+        cmds.shelfButton(toggle_btn, edit=True, backgroundColor=(0, 1, 0))
 toggle_button()
 
 
