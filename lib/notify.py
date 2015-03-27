9c0fd438955856aa0ae81139ec380287eca26f5a
Regnareb <kun_fu_man@hotmail.com>
2015-03-27 15:53:43 +0100

    Update notify.py

--- lib/notify.py
+++ lib/notify.py
@@ -13,8 +13,10 @@ logger = logging.getLogger(__name__)
 
 
 class Notify(object):
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.title = " "
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
 
     def setTitle(self, title):
         self.title = title
@@ -40,16 +42,27 @@ class Notify(object):
     def _constructCmd(self):
         cmd = 'notify-send'
         order = ['title', 'message', 'timeout', 'urgency', 'category', 'icon', 'hint']
-        attributes = {'title': ' "{}"', 'message': ' "{}"', 'timeout': ' -t {}', 'urgency': ' -u {}', 'category': ' -c {}', 'icon': ' -i {}',  'hint': '-h {}'}
+        attributes = {'title': ' "{}"', 'message': ' "{}"', 'timeout': ' -t {}', 'urgency': ' -u {}', 'category': ' -c {}', 'icon': ' -i {}',  'hint': ' -h {}'}
         for attr in order:
             try:
                 value = getattr(self, attr)
                 cmd += attributes[attr].format(value)
-            except AttributeError:    
-                pass   
+            except AttributeError:
+                pass
         logger.debug(cmd)
-        return cmd     
+        return cmd
 
     def send(self):
         cmd = self._constructCmd()
         os.system(cmd)
+
+
+
+
+
+"""notifykde(title='title', text='message')"""
+import dbus
+
+def notifykde(event='warning', fromApp='kde', contexts=[], title='', text='', pixmap=[], actions=[], timeout=0, winId=0, dbus_interface="org.kde.KNotify"):
+    knotify = dbus.SessionBus().get_object("org.kde.knotify", "/Notify")
+    knotify.event(event, fromApp, contexts, title, text, pixmap, actions, timeout, winId, dbus_interface=dbus_interface)
