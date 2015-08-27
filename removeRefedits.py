9df5c6d707c14de500c6bdee6be0f53521ea200e
Regnareb <kun_fu_man@hotmail.com>
2015-08-27 17:56:40 +0200

    Create removeRefedits.py

---
+++ removeRefedits.py
@@ -0,0 +1,31 @@
+lib.lib as tdLib
+
+def getEdits(refNode):
+    editCommands = ['disconnectAttr', 'setAttr', 'connectAttr']
+    refEdits = {i: [] for i in editCommands}
+    for key, val in refEdits.iteritems():
+        val[:] = list(set(cmds.referenceQuery(refNode, editStrings=True, editCommand=key, showDagPath=False)))
+    return refEdits
+
+references = tdLib.getReferences(True)
+ref = references['Yourref']
+ref['editsRef'] = getEdits(ref['refNode'])
+
+['wl[', 'weightList[', 'lodVisibility', 'overrideEnabled', 'output']
+
+pattern = 'wl['
+
+for editType, edits in ref['editsRef'].iteritems():
+    finalList = []
+    for i in edits:
+        if pattern in i:
+            toDel = i.split(' ')[1]
+            finalList.append('Yourref:' + toDel.split('.')[0])
+            cmds.referenceEdit('Yourref:' + toDel, failedEdits=True, successfulEdits=True, editCommand=editType, removeEdits=True) # For the attribute
+
+    # For the node
+    finalList = list(set(finalList))
+    for i in finalList:
+        cmds.referenceEdit(i, failedEdits=True, successfulEdits=True, editCommand=editType, removeEdits=True)
+
+
